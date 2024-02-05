0f8bdb893307d52b88c90e0d33e8a2e6a77cd06b
Jason <34856817@qq.com>
2024-02-05 20:34:04 +0800

    test(用户基础模型): 更新测试用例以验证更新操作
    修改了 TestDBOperations 测试函数,将原来的 db.Save(&m) 更新为 db.Updates(&m)。这是为了验证使用 Updates 方法进行更新操作的正确性。
    
    refactor(用户基础模型): 新增测试验证局部更新功能
    
    新增了 TestDDD 测试函数,用于验证使用 Updates 方法进行局部更新的功能。
    
    该测试函数首先创建一个带有指定 ID 的 UserBasic 对象,然后使用 Create 方法插入到数据库中。接着,它构造了一个只包含想要更新的字段的 UserBasic 对象,并调用 Model(&um).Updates(&um) 进行局部更新。

--- models/models_test.go
+++ models/models_test.go
@@ -36,10 +36,49 @@ func TestDBOperations(t *testing.T) {
 	// 清除表数据
 	db.Exec("DELETE FROM user_basics")
 	// Save
-	db.Save(&m)
+	db.Updates(&m)
 
 	// Read
 	var user UserBasic
 	db.First(&user, "Name = ?", "D42") // 查找 code 字段值为 D42 的记录
 	fmt.Printf("user: %+v\n", user)
+}
+
+func TestDDD(t *testing.T) {
+	// delete 所有数据
+	db.Exec("DELETE FROM user_basic")
+	// 先创建一个 UserBasic
+	name := "D42"
+	pwd := "100"
+	m := UserBasic{
+		Name:     &name,
+		PassWord: &pwd,
+		Model: gorm.Model{
+			ID: 2,
+		},
+	}
+
+	// Create
+	db.Create(&m)
+	// 使用update 更新局部数据
+	// Read
+	var user UserBasic
+	db.First(&user, "ID = ?", 2) // 查找 code 字段值为 D42 的记录
+	// 转成json打印
+	userJson, _ := json.Marshal(user)
+	fmt.Printf("created user: %v\n", string(userJson))
+	nname := "D43"
+	pwd2 := "101"
+	um := UserBasic{
+		Model: gorm.Model{
+			ID: 2,
+		},
+		Name: &nname,
+		PassWord: &pwd2,
+	}
+	db.Model(&um).Updates(&um)
+	db.First(&user, "ID = ?", 2) // 查找 code 字段值为 D42 的记录
+	// 转成json打印
+	userJson, _ = json.Marshal(user)
+	fmt.Printf("updated user: %v\n", string(userJson))
 }
